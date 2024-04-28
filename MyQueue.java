fb84739c105180091dbbabe3647a8b8c67e4ecab
dvn1vl <154106380+dvn1vl@users.noreply.github.com>
2024-04-28 22:46:05 +0500

    Fourth commit for MyQueue

--- MyQueue.java
+++ MyQueue.java
@@ -1,2 +1,35 @@
-public class MyQueue {
+import java.util.NoSuchElementException;
+
+public class MyQueue<E> {
+    private MyList<E> list;
+
+    public MyQueue(MyList<E> list) {
+        this.list = list;
+    }
+
+    public void enqueue(E element) {
+        list.add(element);
+    }
+
+    public E dequeue() {
+        if (list.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return list.remove(0);
+    }
+
+    public E peek() {
+        if (list.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return list.get(0);
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
 }
