0dc3c0c181c3691b04b1ca849784c19f213e9a31
dvn1vl <154106380+dvn1vl@users.noreply.github.com>
2024-04-28 22:46:37 +0500

    Fifth commit for MyQueue

--- MyStack.java
+++ MyStack.java
@@ -1,2 +1,35 @@
-public class MyStack {
+import java.util.EmptyStackException;
+
+public class MyStack<E> {
+    private MyList<E> list;
+
+    public MyStack(MyList<E> list) {
+        this.list = list;
+    }
+
+    public void push(E element) {
+        list.add(element);
+    }
+
+    public E pop() {
+        if (list.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return list.remove(list.size() - 1);
+    }
+
+    public E peek() {
+        if (list.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return list.get(list.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
 }
